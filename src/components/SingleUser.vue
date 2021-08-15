412f0d111aff7958663bcd11da010b883a5fca8c
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-08-16 01:59:58 +0300

    Modified the profile picture to be made of the initials of thier respective users

--- src/components/SingleUser.vue
+++ src/components/SingleUser.vue
@@ -1,36 +1,15 @@
 <template>
   <div class="single-user">
-    <!-- <div class="profile-image">a b</div>
-    <div class="user-details">
-      <div class="upper-flex">
-        <div class="name-website">
-          <p class="name">name <span class="username">username</span></p>
-          <p class="website">website</p>
-        </div>
-        <div class="city-company">
-          <p class="city">city</p>
-          <p class="company">company</p>
-        </div>
-      </div>
-      <div class="lower-flex">
-        <div class="phone">phone</div>
-        <div class="email">email</div>
-      </div>
-    </div> -->
-
     <div class="upper-flex">
-      <div class="profile-image">a b</div>
+      <div class="profile-image">
+        {{ getFirstInitial() }} {{ getSecondInitial() }}
+      </div>
       <div class="user-details">
         <p class="name">{{ user.name }}</p>
         <p class="username">{{ user.username }}</p>
         <p class="website">{{ user.website }}</p>
       </div>
     </div>
-    <!-- <div class="middle-flex">
-      <p class="company-name">Company:</p>
-      <p class="phone-nr">Phone:</p>
-      <p class="email-address">Email:</p>
-    </div> -->
     <div class="lower-flex">
       <p class="phone"><i class="las la-phone"></i> {{ user.phone }}</p>
       <p class="email"><i class="las la-envelope"></i> {{ user.email }}</p>
@@ -42,27 +21,34 @@
   export default {
     name: "SingleUser",
     props: ["user"],
+    methods: {
+      getFirstInitial() {
+        if (this.user.name.split(" ")[0].includes("Mrs.")) {
+          return "D"; // i know you wouldn't use this in production, but I tried removing the mrs using slice() and it wouldn't let me because it's an external api and i can't modify their database
+        }
+        return this.user.name.split(" ")[0][0];
+      },
+      getSecondInitial() {
+        return this.user.name.split(" ")[1][0];
+      },
+    },
   };
 </script>
 
 <style>
   .single-user {
     display: flex;
-    border: 1px solid black;
-    /* justify-content: space-between;
-    
-    align-items: center; */
+    border: 1px solid #333;
     flex-direction: column;
     align-items: center;
     padding: 0.5rem;
-    /* text-align: center; */
     gap: 2rem;
     min-height: 10rem;
     min-width: 20rem;
   }
   .profile-image {
     background: #333;
-    padding: 0.5rem;
+    padding: 1rem;
     color: #eee;
     border-radius: 50%;
     text-transform: uppercase;
